29de11718cbc7686e322c9c734c1a4c5efcc3b82
Semyon Aleksandrovskiy <psifisher@gmail.com>
2020-05-09 06:17:39 +0000

    Add supplier_image_upload, checpoint #2

---
+++ supplier_image_upload.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+import requests
+import os
+
+
+if __name__ == "__main__":
+	url = 'http://localhost/upload/'
+	image_dir = 'supplier-data/images'
+	filelist = os.listdir(image_dir)
+	images = [filename for filename in filelist if os.path.splitext(filename)[1] == '.jpeg']
+	for filename in images:
+		with open(os.path.join(image_dir, filename), 'rb') as opened_file:
+			r = requests.post(url, files={'file': opened_file})
+			print(r.status_code)
